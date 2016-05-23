f982d8469e6ce01aa054d9646d6a462acb587a69
Sean Cross <sean@xobs.io>
2016-05-23 23:37:59 +0800

    wrap-wrap-build: fail if any tests fail
    If even one test returns failure, have wrap-wrap-build return
    a nonzero error.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- wrap-wrap-build.sh
+++ wrap-wrap-build.sh
@@ -9,10 +9,22 @@ done
 echo "fqbns: ${fqbns}"
 echo "inopaths: ${inopaths}"
 
+some_failed=0
 for inopath in ${inopaths}
 do
     for fqbn in ${fqbns}
     do
-        ./wrap-build.sh "${inopath}" "${fqbn}"
+        # Run the test, and let us know if it failed.
+        if ! ./wrap-build.sh "${inopath}" "${fqbn}"
+        then
+            some_failed=1
+        fi
     done
 done
+
+# Exit with an error if at least one test failed.
+if [ ${some_failed} -ne 0 ]
+then
+    exit 1
+fi
+exit 0
